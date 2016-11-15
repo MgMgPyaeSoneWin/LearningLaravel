4d50a8bcb45c890e2977a0f502d0ce67bddf89d0
Pyae Sone <mgmgpyaesonewin@gmail.com>
2016-11-15 08:40:47 +0630

    $error Vairable

---
+++ resources/views/errors/errors.blade.php
@@ -0,0 +1,13 @@
+@if (count($error) > 0)
+    <!-- Form Error List -->
+    <div class="alert alert-danger">
+        <strong>Woops! Something went wrong!</strong>
+        <br>
+        <br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
