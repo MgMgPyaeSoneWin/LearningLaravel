492f254e470aca0a2a879652dc64e23c73894ca0
Pyae Sone <mgmgpyaesonewin@gmail.com>
2016-11-15 07:15:47 +0630

    Defining Task UI

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+        {{--CSS and JS--}}
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                {{--Navbar Contents--}}
+            </nav>
+        </div>
+
+        @yield('content');
+    </body>
+</html>
